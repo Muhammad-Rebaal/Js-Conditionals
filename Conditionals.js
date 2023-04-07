67cd35f7c0a79c6c2e34b511ccf8d63d72176759
Muhammad-Rebaal <122050424+Muhammad-Rebaal@users.noreply.github.com>
2023-04-07 15:49:39 +0500

    Add files via upload

---
+++ Conditionals.js
@@ -0,0 +1,14 @@
+                                        //Conditionals
+
+let person_age = 100
+// if (person_age<=10) {
+//  console.log("You are not able to buy the plot!")   
+// }
+// else if(person_age<=18){
+//     console.log("You are also not able to buy the plot!Because you are a teenager")
+
+// }
+// else{
+//     console.log("You are able to buy the plot!")
+// }
+let c = person_age<=10?console.log("You are not able to buy the plot!"):person_age<=18?console.log("You are also not able to buy the plot!Because you are a teenager"):console.log("You are able to buy the plot!")
